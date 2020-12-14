baec3db561dc9711502cfe0cd7dd4a156676ce9e
tkmoney11 <tylerkim@outlook.com>
2020-12-14 00:47:10 -0800

    Added password changing functionality

--- src/pages/SettingsPage/SettingsPage.js
+++ src/pages/SettingsPage/SettingsPage.js
@@ -1,4 +1,4 @@
-import { Axios } from 'axios'
+import Axios from 'axios'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import './SettingsPage.css'
@@ -9,14 +9,21 @@ export function SettingsPage() {
         return <Redirect to="/"></Redirect>
     }
     const changePassword = async (password) => {
+        let result
         try {
+            result = await Axios.get(
+                `http://3.135.225.25:8080/Project0/FindUserById?id=1`
+            )
         } catch (e) {
             console.log('error retrieving user')
+            console.log(e)
         }
+
+        const email = result.data[0].email
 
         try {
             const result = await Axios.post(
-                `http://3.135.225.25:8080/Project0/ChangePassword?email=${store.email}&password=${password}`,
+                `http://3.135.225.25:8080/Project0/ChangePassword?email=${email}&password=${password}`,
                 {
                     headers: {
                         'Access-Control-Allow-Origin': '*',
@@ -28,16 +35,24 @@ export function SettingsPage() {
             console.log(result) // full data + meta data
             console.log(result.data) // full data
             // console.log(result.data[0]) // individual entry
+            alert('Successfully changed password!!')
         } catch (e) {
             console.log(e.response)
+            alert('error changing password')
         }
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         const data = new FormData(e.target)
-        console.log(data.get('password'))
-        console.log(data.get('password-confirmation'))
+        console.log(data)
+        const password = data.get('password')
+        const confirmedPassword = data.get('password-confirmation')
+        if (password === confirmedPassword) {
+            await changePassword(password)
+        } else {
+            alert("Your passwords don't match. Please Re-enter them")
+        }
     }
 
     return (
@@ -46,7 +61,7 @@ export function SettingsPage() {
                 <div>
                     <form onSubmit={handleSubmit}>
                         <label htmlFor="password">Password</label>
-                        <input id="password" name="email" type="password" />
+                        <input id="password" name="password" type="password" />
                         <br />
                         <label htmlFor="password-confirmation">
                             Confirm Password
