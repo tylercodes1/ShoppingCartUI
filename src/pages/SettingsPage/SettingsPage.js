0cf2c7d5d053e4b48bc8a0f72f496e8fe3c4a520
tkmoney11 <tylerkim@outlook.com>
2020-12-13 22:01:13 -0800

    Initial Settings Page Changes

--- src/pages/SettingsPage/SettingsPage.js
+++ src/pages/SettingsPage/SettingsPage.js
@@ -1,6 +1,7 @@
 import { Axios } from 'axios'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import './SettingsPage.css'
 
 export function SettingsPage() {
     const store = useSelector((store) => store)
@@ -26,11 +27,27 @@ export function SettingsPage() {
             console.log(e.response)
         }
     }
+
+    const handleSubmit = () => {}
+
     return (
         <div className="settings-page">
-            <button onClick={() => console.log('hello')}>
-                Change Password
-            </button>
+            <div className="settings-box">
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">email</label>
+                        <input id="email" name="email" type="text" />
+                        <br />
+                        <label htmlFor="password">password</label>
+                        <input id="password" type="password" name="password" />
+                        <br />
+                        <button>Change Password</button>
+                    </form>
+                </div>
+                <h1 className="logout" onClick={() => console.log('logout')}>
+                    Logout
+                </h1>
+            </div>
         </div>
     )
 }
