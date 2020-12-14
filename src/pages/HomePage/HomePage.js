b4cfc8c98665f903c9da1d1ef753d190f5b77c0e
tkmoney11 <tylerkim@outlook.com>
2020-12-13 22:00:17 -0800

    HomePage Changes

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import './HomePage.css'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt'
+import { Link } from 'react-router-dom'
 
 export function HomePage() {
     const store = useSelector((store) => store)
@@ -10,8 +12,20 @@ export function HomePage() {
 
     return (
         <div className="home-page">
-            <div className="announcement">
-                Welcome to your personalized shopping cart!
+            <div className="home-content">
+                <h1 className="welcome">
+                    Welcome to your personalized shopping experience
+                </h1>
+                <p className="welcome-subtitle">Click below to begin</p>
+                <Link
+                    to="/products"
+                    style={{ textDecoration: 'none', alignSelf: 'flex-end' }}
+                >
+                    <button className="shop-now">
+                        <p className="shop-now-text">Shop Now</p>
+                        <ArrowRightAltIcon fontSize="small" />
+                    </button>
+                </Link>
             </div>
         </div>
     )
