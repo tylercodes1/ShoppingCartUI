16cc78ee31daca3026506402e1909bc19917c9e8
tkmoney11 <tylerkim@outlook.com>
2020-12-13 22:00:33 -0800

    Initial ProductPage changes

--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -1,22 +1,12 @@
 import Axios from 'axios'
 import { React, useEffect, useState } from 'react'
 import './ProductsPage.css'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ProductsPage() {
     const [productState, setProductState] = useState([])
-
-    const showAllProducts = async () => {
-        try {
-            const result = await Axios.get(
-                'http://3.135.225.25:8080/Project0/ShowProductList'
-            )
-            console.log(result)
-            console.log(result.data)
-        } catch (e) {
-            console.log(e)
-            console.log(e.response)
-        }
-    }
     useEffect(async () => {
         // const result = await axios(
         //     'http://3.135.225.25:8080/Project0/Login?email=admin2@gmail.com&password=1234'
@@ -40,12 +30,23 @@ export default function ProductsPage() {
         fetchData()
     }, [])
 
+    const addToShoppingCart = () => {
+        alert('Added to shopping cart!')
+        // return <Redirect to="/shopping-cart"></Redirect>
+    }
+
     const buildProducts = (product) => {
         return (
             <div className="product">
                 <div>{product.prductPrice}</div>
                 <div>{product.productDescription}</div>
                 <div>{product.productName}</div>
+                <Link to="shopping-cart">
+                    <AddCircleIcon
+                        className="add-btn"
+                        onClick={() => addToShoppingCart()}
+                    ></AddCircleIcon>
+                </Link>
             </div>
         )
     }
