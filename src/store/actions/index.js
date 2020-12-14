84af96a21c8585bc67a9de69b1c3cff6d5547bf2
tkmoney11 <tylerkim@outlook.com>
2020-12-13 23:18:28 -0800

    added userid to global state

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -16,10 +16,49 @@ const loginAPI = async (email, password) => {
     }
 }
 
+const getUser = async (email, password) => {
+    try {
+        const result = await Axios.get(
+            'http://3.135.225.25:8080/Project0/ShowAllUsers',
+            {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+
+        console.log(result) // full data + meta data
+        console.log(result.data) // full data
+        const tmp = result.data.filter(
+            (el) => el.email === email && el.password === password
+        )
+        if (tmp.length > 0) {
+            return tmp[0]
+        } else {
+            return 'error'
+        }
+    } catch (e) {
+        console.log(e)
+        return 'error'
+    }
+}
+
 // TODO need to put more sophisticated credentials in Browser
-export const login = async (username, password) => {
-    const resp = await loginAPI(username, password)
+export const login = async (email, password) => {
+    const resp = await loginAPI(email, password)
     console.log(resp)
+    if (resp.includes('You have logged in')) {
+        const user = await getUser(email, password)
+        if (user !== 'error') {
+            return {
+                type: LOGIN,
+                isLoggedIn: true,
+                role: resp.includes('Manager') ? 'Manager' : 'Customer',
+                userId: user.id,
+            }
+        }
+    }
     return {
         type: LOGIN,
         isLoggedIn: resp.includes('You have logged in') ? true : false,
