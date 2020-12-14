40a1d298a1ff4bf2bb96f461dd426764b35d5b52
tkmoney11 <tylerkim@outlook.com>
2020-12-13 22:28:46 -0800

    Fleshing out shopping cart page

--- src/pages/ShoppingCartPage/ShoppingCartPage.js
+++ src/pages/ShoppingCartPage/ShoppingCartPage.js
@@ -1,5 +1,23 @@
-import { Divider } from '@material-ui/core'
+import { React } from 'react'
+import './ShoppingCartPage.css'
+import Axios from 'axios'
 
 export function ShoppingCartPage() {
-    return <div>shoppingcart</div>
+    const getProductsByUserId = async (id) => {
+        try {
+            const result = await Axios.get(
+                ` http://3.135.225.25:8080/Project0/productWithStatusByUserId?id=${id}`
+            )
+            console.log(result)
+            console.log(result.data)
+        } catch (e) {
+            console.log(e.response)
+        }
+    }
+    return (
+        <div className="shopping-cart-page">
+            {/* id will be globally stored state */}
+            <button onClick={() => getProductsByUserId(2)}>show</button>
+        </div>
+    )
 }
